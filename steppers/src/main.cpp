470f10674c611d598539a1144f515d827aadb0d4
MalyshevaAnna2020 <75412506+MalyshevaAnna2020@users.noreply.github.com>
2022-07-13 19:38:11 +0300

    removed unused variables, changed durations

--- steppers/src/main.cpp
+++ steppers/src/main.cpp
@@ -9,46 +9,44 @@
 #define K 200
 
 int target = (int) ((target_s*K)/(2*M_PI*r));// Кол-во шагов для достижения нужныого пути 
-int present = 0; // Текущий шаг
 int count = 0; // Для повтора пути
-int mode = 0; // 0 - наверх, 1 - вниз
 
 
 // t = target / 2 => acceleration & deceleration
 int t = (int) (target / 2);
 
-long long n = 300000;
+long long n = 60000;
 
 void acceleration()
 {
   digitalWrite(STEP_1, HIGH);
   digitalWrite(STEP_2, HIGH);
-  delayMicroseconds(2000);
+  delayMicroseconds(n);
   digitalWrite(STEP_1, LOW);
   digitalWrite(STEP_2, LOW);
-  delay(n);
-  n -= 2000;
+  delayMicroseconds(n);
+  n -= 31;
 }
 
 void deceleration()
 {
   digitalWrite(STEP_1, HIGH);
   digitalWrite(STEP_2, HIGH);
-  delayMicroseconds(2000);
+  delayMicroseconds(n);
   digitalWrite(STEP_1, LOW);
   digitalWrite(STEP_2, LOW);
-  delay(n);
-  n += 2000;
+  delayMicroseconds(n);
+  n += 31;
 }
 
 void constant_speed()
 {
   digitalWrite(STEP_1, HIGH);
   digitalWrite(STEP_2, HIGH);
-  delayMicroseconds(2000);
+  delayMicroseconds(50000);
   digitalWrite(STEP_1, LOW);
   digitalWrite(STEP_2, LOW);
-  delayMicroseconds(2000);
+  delayMicroseconds(50000);
 }
 
 void setup() {
@@ -56,19 +54,22 @@ void setup() {
   pinMode(STEP_2, OUTPUT);
 
   // or t = n / 2 000 => acceleration & horizontal line & deceleration
-  if (target > 100) t = 50;
+  if (target > 640) t = 320;
 }
 
 void loop() {
-  if (count < t)
+  if (count < target)
   {
-    acceleration();
-  }
-  else 
-  {
-    if (count > target - t) deceleration();
-    else constant_speed();
-  }
+    if (count < t)
+    {
+      acceleration();
+    }
+    else 
+    {
+      if (count > target - t) deceleration();
+      else constant_speed();
+    }
 
-  count++;
+    count++;
+  }
 }
