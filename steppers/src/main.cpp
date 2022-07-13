82cf49410b813042efc90921c623fd36d396fb8b
MalyshevaAnna2020 <75412506+MalyshevaAnna2020@users.noreply.github.com>
2022-07-13 20:03:13 +0300

    duration changed

--- steppers/src/main.cpp
+++ steppers/src/main.cpp
@@ -15,46 +15,46 @@ int count = 0; // Для повтора пути
 // t = target / 2 => acceleration & deceleration
 int t = (int) (target / 2);
 
-long long n = 60000;
+long long n = 50;
 
 void acceleration()
 {
   digitalWrite(STEP_1, HIGH);
   digitalWrite(STEP_2, HIGH);
-  delayMicroseconds(n);
+  delay(n);
   digitalWrite(STEP_1, LOW);
   digitalWrite(STEP_2, LOW);
-  delayMicroseconds(n);
-  n -= 31;
+  delay(n);
+  n -= 1;
 }
 
 void deceleration()
 {
   digitalWrite(STEP_1, HIGH);
   digitalWrite(STEP_2, HIGH);
-  delayMicroseconds(n);
+  delay(n);
   digitalWrite(STEP_1, LOW);
   digitalWrite(STEP_2, LOW);
-  delayMicroseconds(n);
-  n += 31;
+  delay(n);
+  n += 1;
 }
 
 void constant_speed()
 {
   digitalWrite(STEP_1, HIGH);
   digitalWrite(STEP_2, HIGH);
-  delayMicroseconds(50000);
+  delay(1);
   digitalWrite(STEP_1, LOW);
   digitalWrite(STEP_2, LOW);
-  delayMicroseconds(50000);
+  delay(1);
 }
 
 void setup() {
   pinMode(STEP_1, OUTPUT);
   pinMode(STEP_2, OUTPUT);
 
-  // or t = n / 2 000 => acceleration & horizontal line & deceleration
-  if (target > 640) t = 320;
+  // or t = n / 1 => acceleration & horizontal line & deceleration
+  if (target > 100) t = 50;
 }
 
 void loop() {
